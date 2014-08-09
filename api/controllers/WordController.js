e9291ad76156a9eb728ea39e773f0d0ef429187b
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-08-09 19:12:06 -0400

    -Generated api for Dialog and Word

---
+++ api/controllers/WordController.js
@@ -0,0 +1,11 @@
+/**
+ * WordController
+ *
+ * @description :: Server-side logic for managing words
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
