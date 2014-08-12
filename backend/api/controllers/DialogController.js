391185e1f706042f142c10d4e08c5e5dc0e9f298
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-08-11 21:51:06 -0400

    -Backend now properly searches for the dialog and returns them

--- backend/api/controllers/DialogController.js
+++ backend/api/controllers/DialogController.js
@@ -13,22 +13,25 @@ module.exports = {
         var params = req.params.all(),
             sails_mysql = require('sails-mysql'),
             dialog = sails.models.dialog,
-            query, match_string, against_string;
-        russian_param = req.param('ru');
-        english_param = req.param('en');
-        if(russian_param) {
+            query, sql_param = {};
+        sql_param.russian = req.param('ru');
+        sql_param.english = req.param('en');
+        sql_param.limit = req.param('limit') ? req.param('limit') : 4;
+
+        if(sql_param.russian) {
             match_string = 'ru';
-            against_string = russian_param;
+            against_string = sql_param.russian;
         }
-        if(english_param) {
+        if(sql_param.english) {
             match_string = 'en';
-            against_string = english_param;
+            against_string = sql_param.english;
         }
+        console.log(match_string, against_string);
         query = 'SELECT * FROM dialog d WHERE MATCH(';
         query += match_string;
         query += ') AGAINST (';
         query += sails_mysql.escape(against_string);
-        query += ') ORDER BY RAND() LIMIT 4';
+        query += ') ORDER BY RAND() LIMIT ' + sql_param.limit;
 
         dialog.query(query, function(err, words) {
             return res.send(words);
