711e8cb9591eb8ba4e1b5880f83bb5fc82408339
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-08-19 10:12:54 -0400

    -Adding migrate:safe option to all the models

--- backend/config/models.js
+++ backend/config/models.js
@@ -18,5 +18,6 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
 
+   migrate: 'safe',
    connection: 'myLocalMySQLDatabase'
 };
